eef730ccb5bdf8281c4585c31c711a6837a56bea
boaz <boaz@boaz-XPS-M1530.(none)>
2012-11-28 19:15:43 +0900

    Beginning of chapter 3

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,13 +6,13 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+		$layout = $this->_helper->layout();
+		$layout->setLayout('alternative_layout');
     }
 
     public function indexAction()
     {
-        // action body
+		//$this->_helper->layout()->disableLayout();
     }
-
-
 }
 
